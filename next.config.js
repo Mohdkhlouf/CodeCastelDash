3b64e9cda685af5f255c1d1042b3adc6b4afd2d0
Mohammad Khlouf <mohammad.khlouf@gmail.com>
2024-05-23 14:44:17 +0300

    test 3

--- next.config.js
+++ next.config.js
@@ -2,3 +2,10 @@
 const nextConfig = {};
 
 module.exports = nextConfig;
+
+module.exports = {
+  experimental: {
+    // this includes files from the monorepo base two directories up
+    outputFileTracingRoot: path.join(__dirname, "../../"),
+  },
+};
