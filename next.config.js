3d5768cb23f3b8ba9d443eda76db9c5acc53cd95
Mohammad Khlouf <mohammad.khlouf@gmail.com>
2024-05-24 22:37:39 +0300

    test2

--- next.config.js
+++ next.config.js
@@ -1,31 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  // Your existing configuration
-  reactStrictMode: true, // Example configuration
-  webpack(config, { isServer }) {
-    // Bundle analyzer configuration
-    if (process.env.ANALYZE === "true") {
-      const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
-      config.plugins.push(
-        new BundleAnalyzerPlugin({
-          analyzerMode: "server",
-          analyzerPort: isServer ? 8888 : 8889,
-          openAnalyzer: true,
-        })
-      );
-    }
+const nextConfig = {};
 
-    // Example: Exclude certain modules from serverless functions
-    if (isServer) {
-      config.externals = ["sharp"]; // Replace 'sharp' with other modules if necessary
-    }
-
-    return config;
-  },
-};
-
-const withBundleAnalyzer = require("@next/bundle-analyzer")({
-  enabled: process.env.ANALYZE === "true",
-});
-
-module.exports = withBundleAnalyzer(nextConfig);
+module.exports = nextConfig;
